060bf729827d1b19bbc9de24c1d6d012b807dcd8
Supergrammer <supergrammer@naver.com>
2023-01-02 15:15:23 +0900

    [Fix] vite.config.js base url 수정, #2

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/github-pages/",
+    base: "/github-pages",
 
     plugins: [vue()],
     resolve: {
