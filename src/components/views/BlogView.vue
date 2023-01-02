56f032821b84f193a30b657cadd19392e720c74a
Supergrammer <supergrammer@naver.com>
2023-01-03 08:20:09 +0900

    [Feat] BlogView 테스트 코드 추가, #5

--- src/components/views/BlogView.vue
+++ src/components/views/BlogView.vue
@@ -3,7 +3,9 @@
 </script>
 
 <template>
-    <v-card></v-card>
+    <v-container style="padding-top: 56px;">
+        <v-card height="400vh" > 1232131</v-card>
+    </v-container>
 </template>
 
 <style scoped>
--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import App from "@/App.vue";
 import router from "@/router";
 import vuetify from "@/plugins/vuetify";
 
-import "@mdi/font/css/materialdesignicons.css"
+// import "@mdi/font/css/materialdesignicons.css"
 import "@fortawesome/fontawesome-free/css/all.css"
 
 const app = createApp(App);
