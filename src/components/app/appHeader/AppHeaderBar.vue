7495fd079789f8b8198cc2364cb018aad974d8a3
Supergrammer <supergrammer@naver.com>
2023-01-03 08:19:50 +0900

    [Feat] AppHeaderBar 추가, #4

---
+++ src/components/app/appHeader/AppHeaderBar.vue
@@ -0,0 +1,29 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="pa-1 d-flex align-center">
+        <v-btn icon flat>
+            <v-icon icon="fas fa-bars" />
+        </v-btn>
+
+        <v-spacer />
+        <div @click="$emit('close')">
+            <span v-text="'개발자 BLOG'" style="font-size: 25px;" />
+        </div>
+        <v-spacer />
+
+        <v-btn icon flat>
+            <v-icon icon="fas fa-magnifying-glass" />
+        </v-btn>
+
+        <v-btn icon flat>
+            <v-icon icon="fas fa-ellipsis-vertical" />
+        </v-btn>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
