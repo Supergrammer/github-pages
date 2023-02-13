15134553a6559d716302698f1b3d662cc97e9e03
Supergrammer <supergrammer@naver.com>
2023-02-13 10:43:53 +0900

    [Feat] App.vue 뷰 수정 및 background image 적용, #4

--- src/App.vue
+++ src/App.vue
@@ -3,27 +3,104 @@ import AppHeader from "@/components/app/AppHeader.vue";
 </script>
 
 <template>
-    <v-app id="app">
-        <app-header />
-        <router-view />
-    </v-app>
+	<v-app full-height>
+		<div class="app-background" />
+		<AppHeader />
+
+		<v-container class="app-content">
+			<RouterView />
+		</v-container>
+	</v-app>
 </template>
 
 <style scoped>
-@import url('https://fonts.googleapis.com/css2?family=Hahmlet:wght@300;400&display=swap');
+.app-background {
+	position: fixed;
 
+	background-image: url("@/assets/images/appBackground.jpg");
 
-#app {
-    /* IE and Edge */
-    -ms-overflow-style: none;
-    /* Firefox */
-    scrollbar-width: none;
+	background-position: center;
+	background-repeat: no-repeat;
+	background-size: cover;
 
-    font-family: "Hahmlet";
+	width: 100%;
+	height: 100%;
+	opacity: 0.75;
+
+	filter: blur(5px) grayscale(100%);
+	transition: filter 0.5s;
 }
 
-#app::-webkit-scrollbar {
-    /* Chrome, Safari, Opera */
-    display: none;
+.app-background:hover {
+	filter: none;
 }
+
+.app-content {
+	width: 75%;
+
+	padding: 0;
+	padding-top: 64px;
+}
+
+/* header {
+    line-height: 1.5;
+    max-height: 100vh;
+}
+
+.logo {
+    display: block;
+    margin: 0 auto 2rem;
+}
+
+nav {
+    width: 100%;
+    font-size: 12px;
+    text-align: center;
+    margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+    color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+    background-color: transparent;
+}
+
+nav a {
+    display: inline-block;
+    padding: 0 1rem;
+    border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+    border: 0;
+}
+
+@media (min-width: 1024px) {
+    header {
+        display: flex;
+        place-items: center;
+        padding-right: calc(var(--section-gap) / 2);
+    }
+
+    .logo {
+        margin: 0 2rem 0 0;
+    }
+
+    header .wrapper {
+        display: flex;
+        place-items: flex-start;
+        flex-wrap: wrap;
+    }
+
+    nav {
+        text-align: left;
+        margin-left: -1rem;
+        font-size: 1rem;
+
+        padding: 1rem 0;
+        margin-top: 1rem;
+    }
+} */
 </style>
