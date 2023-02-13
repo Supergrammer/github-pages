877860c92d023a1213d9a2c590214e20396c01d7
Supergrammer <supergrammer@naver.com>
2023-02-13 10:42:43 +0900

    [Feat] AppHeader theme change 스위치 개발, #4

---
+++ src/components/app/appHeader/AppHeaderToggleThemeButton.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { computed, ref } from "vue";
+import { useTheme } from "vuetify";
+
+const theme = useTheme();
+const currentTheme = ref(theme.global.name);
+
+const themeSwitch = computed({
+	get: () => currentTheme.value === "dark",
+	set: value => (currentTheme.value = value ? "dark" : "light"),
+});
+</script>
+
+<template>
+	<div>
+		<v-switch
+			v-model="themeSwitch"
+			inset
+			hide-details
+			true-icon="far fa-moon"
+			false-icon="fas fa-moon"
+			color="yellow"
+		/>
+	</div>
+</template>
+
+<style scoped></style>
