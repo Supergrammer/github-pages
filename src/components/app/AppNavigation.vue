efa3cd1c1de13a7ec36ccfd3c2ed3761efa832c1
Supergrammer <supergrammerkim@gmail.com>
2023-02-21 20:44:13 +0900

    [Feat] AppNavigation 컴포넌트 추가, #8

---
+++ src/components/app/AppNavigation.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { inject } from "vue";
+
+import AppNavigationProfile from "./appNavigation/AppNavigationProfile.vue";
+import AppNavigationMenuList from "./appNavigation/AppNavigationMenuList.vue";
+import AppNavigationSocialMediaButtons from "./appNavigation/AppNavigationSocialMediaButtons.vue";
+import AppNavigationCopyright from "./appNavigation/AppNavigationCopyright.vue";
+
+const drawer = inject("drawer");
+</script>
+
+<template>
+	<v-navigation-drawer v-model="drawer" temporary :width="300" :elevation="7">
+		<AppNavigationProfile />
+		<AppNavigationMenuList />
+		<v-divider />
+		<AppNavigationSocialMediaButtons />
+		<AppNavigationCopyright />
+	</v-navigation-drawer>
+</template>
+
+<style scoped>
+.app-navigation {
+	z-index: 1;
+}
+</style>
