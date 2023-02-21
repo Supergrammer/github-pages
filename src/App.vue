94ae643e9292e55a645f03b973cb5a2a89df38b1
Supergrammer <supergrammerkim@gmail.com>
2023-02-21 20:43:10 +0900

    [Feat] AppNavigation 추가, #8

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
-import AppHeader from "@/components/app/AppHeader.vue";
+import AppHeader from "./components/app/AppHeader.vue";
+import AppNavigation from "./components/app/AppNavigation.vue";
 </script>
 
 <template>
 	<v-app full-height>
 		<div class="app-background" />
 		<AppHeader />
+		<AppNavigation />
 
 		<v-container class="app-content">
 			<RouterView />
@@ -41,66 +43,4 @@ import AppHeader from "@/components/app/AppHeader.vue";
 	padding: 0;
 	padding-top: 64px;
 }
-
-/* header {
-    line-height: 1.5;
-    max-height: 100vh;
-}
-
-.logo {
-    display: block;
-    margin: 0 auto 2rem;
-}
-
-nav {
-    width: 100%;
-    font-size: 12px;
-    text-align: center;
-    margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-    color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-    background-color: transparent;
-}
-
-nav a {
-    display: inline-block;
-    padding: 0 1rem;
-    border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-    border: 0;
-}
-
-@media (min-width: 1024px) {
-    header {
-        display: flex;
-        place-items: center;
-        padding-right: calc(var(--section-gap) / 2);
-    }
-
-    .logo {
-        margin: 0 2rem 0 0;
-    }
-
-    header .wrapper {
-        display: flex;
-        place-items: flex-start;
-        flex-wrap: wrap;
-    }
-
-    nav {
-        text-align: left;
-        margin-left: -1rem;
-        font-size: 1rem;
-
-        padding: 1rem 0;
-        margin-top: 1rem;
-    }
-} */
 </style>
