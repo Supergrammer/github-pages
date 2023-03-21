42a65cee657ad5985ba6ebe979011d36615723f1
Supergrammer <supergrammerkim@gmail.com>
2023-03-21 16:39:08 +0900

    [Feat] 기본 Iconset 을 material design icons 로 변경, #9

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,6 +4,7 @@ import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
 
 import "vuetify/styles";
+import "@mdi/font/css/materialdesignicons.css";
 
 const vuetify = createVuetify({
 	components,
