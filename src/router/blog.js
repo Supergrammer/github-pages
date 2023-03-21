4fb4aa05f9d1737533dcb9b09b273b1927726958
Supergrammer <supergrammerkim@gmail.com>
2023-03-21 16:38:41 +0900

    [Chore] router 정보 초기화 및 모듈 분리, #9

---
+++ src/router/blog.js
@@ -0,0 +1,9 @@
+const blogRoutes = [
+	{
+		path: "",
+		name: "blog-home",
+		component: null,
+	},
+];
+
+export default blogRoutes;
--- src/router/index.js
+++ src/router/index.js
@@ -1,40 +1,16 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-import BlogPage from "@/components/pages/BlogPage.vue";
-import ResumePage from "@/components/pages/ResumePage.vue";
-import ErrorPage from "@/components/pages/ErrorPage.vue";
-
 const routes = [
 	{
 		path: "/",
 		name: "main",
 		redirect: { name: "blog" },
-		// component: () => import("@/components/views/MapView.vue"),
-	},
-	{
-		path: "/blog",
-		name: "blog",
-		component: BlogPage,
-	},
-	{
-		path: "/resume",
-		name: "resume",
-		component: ResumePage,
-	},
-	{
-		path: "/:pathMatch(.*)*",
-		name: "error",
-		component: ErrorPage,
 	},
 ];
 
 const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
 	routes: routes,
-});
-
-router.beforeEach((to, from, next) => {
-	next(!to.redirectedFrom && to.name === "error" ? { path: to.path } : null);
 });
 
 export default router;
